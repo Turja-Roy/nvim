5e9e42ad744e69341582dfc2ea7d455b69bc6895
Turja Roy <turja.roy@outlook.com>
2024-08-12 09:52:44 +0600

    installed telescope-ui-select

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -71,4 +71,19 @@ return {
         end
     },
 
+    -- UI for telescope
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+        config = function ()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {}
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    }
+
 }
