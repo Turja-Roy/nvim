e100b2cc1de966c83401a9883965929454f7129b
Turja Roy <turja.roy@outlook.com>
2025-05-18 14:04:36 -0500

    error-messages

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -78,15 +78,16 @@ return {
                 ensure_installed = vim.tbl_keys(servers),
             }
 
-            mason_lspconfig.setup_handlers {
-                function(server_name)
-                    require('lspconfig')[server_name].setup {
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                        settings = servers[server_name],
-                    }
-                end,
-            }
+            -- No idea what it does. Commented out cz it was giving error on startup
+            -- mason_lspconfig.setup_handlers {
+            --     function(server_name)
+            --         require('lspconfig')[server_name].setup {
+            --             capabilities = capabilities,
+            --             on_attach = on_attach,
+            --             settings = servers[server_name],
+            --         }
+            --     end,
+            -- }
         end,
     },
 }
