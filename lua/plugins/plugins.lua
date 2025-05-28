816868df191e8492234f32e127de8765137a16fd
Turja Roy <turja.roy@outlook.com>
2025-05-28 12:53:14 -0500

    neoscroll

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -84,4 +84,18 @@ return {
         dependencies = { "MunifTanjim/nui.nvim" },
         opts = {},
     },
+
+    {
+        "karb94/neoscroll.nvim",
+        opts = {
+            mappings = {
+                '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>',
+                'zt', 'zz', 'zb',
+            },
+            hide_cursor = true,
+            stop_eof = false,
+            respect_scrolloff = false,
+            cursor_scrolls_alone = false,
+        },
+    },
 }
