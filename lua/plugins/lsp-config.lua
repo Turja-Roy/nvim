fdcd1fa331cc00381e71322982e251661d3608af
Turja Roy <turja.roy@outlook.com>
2026-02-10 15:34:32 -0600

    lsp updates

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,13 +9,19 @@ return {
         },
 
         config = function ()
-            local on_attach = function(_, bufnr)
+            -- Setup neovim lua configuration
+            require('neodev').setup()
 
+            -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+            -- Create LSP keybindings when a buffer attaches to an LSP
+            local on_attach = function(_, bufnr)
                 local nmap = function(keys, func, desc)
                     if desc then
                         desc = 'LSP: ' .. desc
                     end
-
                     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
                 end
 
@@ -23,17 +29,15 @@ return {
                 nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
                 nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-                nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
                 nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
                 nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-                nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-                nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                nmap('<leader>ds', vim.lsp.buf.document_symbol, '[D]ocument [S]ymbols')
+                nmap('<leader>ws', vim.lsp.buf.workspace_symbol, '[W]orkspace [S]ymbols')
 
-                -- See `:help K` for why this keymap
                 nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
                 nmap('<C-A-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-                -- Lesser used LSP functionality
                 nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
                 nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
                 nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
@@ -47,48 +51,98 @@ return {
                 end, { desc = 'Format current buffer with LSP' })
             end
 
-            --  Add any additional override configuration in the following tables. They will be passed to
-            --  the `settings` field of the server config. You must look up that documentation yourself.
-            local servers = {
-                -- clangd = {},
-                -- gopls = {},
-                -- pyright = {},
-                -- rust_analyzer = {},
-                -- tsserver = {},
+            -- Configure R Language Server using native Neovim 0.11 API
+            vim.lsp.config('r_language_server', {
+                cmd = { 'R', '--slave', '-e', 'languageserver::run()' },
+                filetypes = { 'r', 'rmd', 'quarto' },
+                root_dir = vim.fs.root(0, {'.git', '.Rproj.user', '_quarto.yml'}),
+                settings = {
+                    r = {
+                        lsp = {
+                            diagnostics = true,
+                            rich_documentation = true,
+                        },
+                    },
+                },
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
 
-                -- lua_ls = {
-                --     Lua = {
-                --         workspace {
-                --             library = vim.api.nvim_get_runtime_file("", true),
-                --         },
-                --     },
-                -- },
-            }
+            -- Configure Quarto LSP
+            vim.lsp.config('quarto-lsp', {
+                filetypes = { 'quarto', 'markdown' },
+                root_dir = vim.fs.root(0, {'.git', '_quarto.yml', '_quarto.yaml'}),
+                settings = {
+                    quarto = {
+                        diagnostics = {
+                            enable = true,
+                        },
+                    },
+                },
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
 
-            -- Setup neovim lua configuration
-            require('neodev').setup()
+            -- Configure Python LSP (basedpyright)
+            vim.lsp.config('basedpyright', {
+                filetypes = { 'python', 'quarto' },
+                root_dir = vim.fs.root(0, {'.git', 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile'}),
+                settings = {
+                    basedpyright = {
+                        analysis = {
+                            typeCheckingMode = "basic",
+                            autoSearchPaths = true,
+                            useLibraryCodeForTypes = true,
+                            diagnosticMode = "openFilesOnly",
+                        },
+                    },
+                },
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
 
-            -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+            -- Configure Marksman (Markdown LSP)
+            vim.lsp.config('marksman', {
+                filetypes = { 'markdown', 'quarto', 'rmd' },
+                root_dir = vim.fs.root(0, {'.git', '.marksman.toml'}),
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
 
-            -- Ensure the servers above are installed
-            local mason_lspconfig = require 'mason-lspconfig'
+            -- Example configurations for other servers (commented out)
+            -- Uncomment and configure as needed:
+            
+            -- vim.lsp.config('lua_ls', {
+            --     settings = {
+            --         Lua = {
+            --             runtime = { version = 'LuaJIT' },
+            --             workspace = {
+            --                 library = vim.api.nvim_get_runtime_file("", true),
+            --                 checkThirdParty = false,
+            --             },
+            --             diagnostics = { globals = { 'vim' } },
+            --         },
+            --     },
+            --     capabilities = capabilities,
+            --     on_attach = on_attach,
+            -- })
 
-            mason_lspconfig.setup {
-                ensure_installed = vim.tbl_keys(servers),
-            }
+            -- vim.lsp.config('clangd', {
+            --     capabilities = capabilities,
+            --     on_attach = on_attach,
+            -- })
 
-            -- No idea what it does. Commented out cz it was giving error on startup
-            -- mason_lspconfig.setup_handlers {
-            --     function(server_name)
-            --         require('lspconfig')[server_name].setup {
-            --             capabilities = capabilities,
-            --             on_attach = on_attach,
-            --             settings = servers[server_name],
-            --         }
-            --     end,
-            -- }
+            -- Setup Mason and mason-lspconfig
+            require('mason').setup()
+            require('mason-lspconfig').setup({
+                ensure_installed = { 
+                    'r_language_server',
+                    'quarto-lsp',
+                    'basedpyright',
+                    'marksman',
+                },
+                automatic_enable = true, -- Automatically enable installed servers
+            })
         end,
     },
 }
